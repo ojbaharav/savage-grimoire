64bfe2975b605393220e34bf80eca05c5799338d
omerbv <omerbv@gmail.com>
2025-10-30 21:21:19 +0000

    improve unselected state & use custom select component

--- src/components/FilterPanel/filters/DurationFilter.tsx
+++ src/components/FilterPanel/filters/DurationFilter.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import {
   Box,
   Chip,
-  Select,
   MenuItem,
-  FormControl,
-  InputLabel,
 } from '@mui/material';
 import { type SelectChangeEvent } from '@mui/material/Select';
 import { type Filters } from '../../../types/filters';
+import CustomSelect from '../../CustomSelect';
 
 interface DurationFilterProps {
   filters: Filters;
@@ -21,47 +19,38 @@ const DurationFilter: React.FC<DurationFilterProps> = ({ filters, durations, onF
     const { value } = event.target;
     const newValues = typeof value === 'string' ? value.split(',') : value;
 
-    if (newValues.includes('Any')) {
-      const lastSelectedItem = newValues[newValues.length - 1];
-      
-      if (lastSelectedItem === 'Any') {
-        onFilterChange({ ...filters, duration: [] });
-      } else {
-        onFilterChange({ ...filters, duration: newValues.filter(v => v !== 'Any') });
-      }
-    } else {
-      onFilterChange({ ...filters, duration: newValues });
-    }
+    onFilterChange({ ...filters, duration: newValues });
   };
 
-  const durationOptions = ['Any', ...durations];
 
   return (
     <Box mt={2}>
-      <FormControl fullWidth>
-        <InputLabel>Duration</InputLabel>
-        <Select
-          multiple
-          value={filters.duration?.length ? filters.duration : ['Any']}
-          onChange={handleDurationChange}
-          renderValue={selected => (
+      <CustomSelect
+        labelId="duration-select-label"
+        id="duration-select"
+        multiple
+        value={filters.duration?.length ? filters.duration : []}
+        onChange={handleDurationChange}
+        label="Duration"
+        renderValue={selected => {
+          if (selected.length === 0) {
+            return undefined; // This makes the InputLabel act as a placeholder when nothing is selected
+          }
+          return (
             <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {(selected.length === 0 || (selected.length === 1 && selected[0] === 'Any')) 
-                ? <Chip key="any" label="Any" /> 
-                : selected.filter(val => val !== 'Any').map(value => (
-                    <Chip key={value} label={value} />
-                  ))
-              }
+              {selected.map(value => (
+                <Chip key={value} label={value} />
+              ))}
             </Box>
-          )}
-        >
-          {durationOptions.map(duration => (
-            <MenuItem key={duration} value={duration}>
-              {duration}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+          )
+        }}
+      >
+        {durations.map(duration => (
+          <MenuItem key={duration} value={duration}>
+            {duration}
+          </MenuItem>
+        ))}
+      </CustomSelect>
     </Box>
   );
 };
