be41bf10c60192488d176485bb6f084df86fc44d
omerbv <omerbv@gmail.com>
2025-10-30 21:36:54 +0000

    convert cbox filter to select

--- src/components/FilterPanel/filters/DomainFilter.tsx
+++ src/components/FilterPanel/filters/DomainFilter.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {
   Box,
-  Checkbox,
-  FormControlLabel,
+  Chip,
+  MenuItem,
   Typography,
 } from '@mui/material';
+import { type SelectChangeEvent } from '@mui/material/Select';
 import { type Filters } from '../../../types/filters';
+import CustomSelect from '../../CustomSelect';
 
 interface DomainFilterProps {
   filters: Filters;
@@ -14,34 +16,41 @@ interface DomainFilterProps {
 }
 
 const DomainFilter: React.FC<DomainFilterProps> = ({ filters, domains, onFilterChange }) => {
-  const handleCheckboxChange = (domain: string) => {
-    const currentDomains = filters.domain || [];
-    let newDomains: string[];
+  const handleDomainChange = (event: SelectChangeEvent<string[]>) => {
+    const { value } = event.target;
+    const newValues = typeof value === 'string' ? value.split(',') : value;
 
-    if (currentDomains.includes(domain)) {
-      newDomains = currentDomains.filter(item => item !== domain);
-    } else {
-      newDomains = [...currentDomains, domain];
-    }
-
-    onFilterChange({ ...filters, domain: newDomains });
+    onFilterChange({ ...filters, domain: newValues });
   };
 
   return (
     <Box mt={2}>
-      <Typography variant="subtitle1">Domain</Typography>
-      {domains.map(domain => (
-        <FormControlLabel
-          key={domain}
-          control={
-            <Checkbox
-              checked={filters.domain?.includes(domain) || false}
-              onChange={() => handleCheckboxChange(domain)}
-            />
+      <CustomSelect
+        labelId="domain-select-label"
+        id="domain-select"
+        multiple
+        value={filters.domain?.length ? filters.domain : []}
+        onChange={handleDomainChange}
+        label="Domain"
+        renderValue={selected => {
+          if (selected.length === 0) {
+            return undefined; // This makes the InputLabel act as a placeholder when nothing is selected
           }
-          label={domain}
-        />
-      ))}
+          return (
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+              {selected.map(value => (
+                <Chip key={value} label={value} />
+              ))}
+            </Box>
+          )
+        }}
+      >
+        {domains.map(domain => (
+          <MenuItem key={domain} value={domain}>
+            {domain}
+          </MenuItem>
+        ))}
+      </CustomSelect>
     </Box>
   );
 };
